214de0bde3de19dbdbf815cccd3fb40d42046d71
markman4897 <lisicic.mark@gmail.com>
2021-06-12 17:15:47 +0200

    first commit

---
+++ scenes/Ball.gd
@@ -0,0 +1,26 @@
+extends RigidBody2D
+
+# where the ball should slowly go to
+var pos := self.global_position
+const SPD := 10
+var mouse_in_ball := false
+var moving := false
+
+func _ready():
+	self.gravity_scale = 0
+	self.linear_damp = 10
+
+func _input(event) -> void:
+	# if mouse down run move func  
+	if (Input.is_action_pressed("left_click") and (event is InputEventMouseButton or InputEventMouseMotion)) and (moving or mouse_in_ball):
+		moving = true
+		pos = event.position
+		self.linear_velocity = (pos - self.global_position) * 10
+	else:
+		moving = false
+
+func _on_Ball_mouse_entered() -> void:
+	mouse_in_ball = true
+
+func _on_Ball_mouse_exited() -> void:
+	mouse_in_ball = false
