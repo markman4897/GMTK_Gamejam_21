20a7503e7a782e884851db283d6dafc3a0fa12b6
markman4897 <lisicic.mark@gmail.com>
2021-06-15 01:39:23 +0200

    updated touch support for mobile

--- scenes/Ball.gd
+++ scenes/Ball.gd
@@ -7,13 +7,16 @@ var mouse_in_ball := false
 var moving := false
 
 func _input(event) -> void:
-	# if mouse down run move func  
-	if (Input.is_action_pressed("left_click") and (event is InputEventMouseButton or InputEventMouseMotion or InputEventScreenTouch or InputEventScreenDrag)) and (moving or mouse_in_ball):
-		moving = true
-		pos = event.position
-		self.linear_velocity = (pos - self.global_position) * 10
-	else:
-		moving = false
+	
+	# this should be looked at closer and redone  
+	if Input.is_action_pressed("left_click") and (moving or mouse_in_ball):
+			if event is InputEventScreenTouch:
+#				print("ping") # this shit only pings when you let go of the screen?!
+				moving = false
+			elif event is InputEventScreenDrag:
+				moving = true
+				pos = event.position
+				self.linear_velocity = (pos - self.global_position) * SPD
 
 func _on_Ball_mouse_entered() -> void:
 	mouse_in_ball = true
